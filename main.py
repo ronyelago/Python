2edc3874c00fb8b5cd588bbba7c98bfc8fec7cde
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-12 03:54:25 -0300

    lists

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 texto = open(r"C:\Workspace\Python\Python\arqs-06\arq4.in").readlines()
+
 r = int(texto[0])
+texto.pop(0)
 
 assassinos = []
 vitimas = []
 detetives = []
 
-for i in range(r) :
-    assassinos = texto[1]
-    detetives = texto[2]
-    vitimas = texto[3]
+i = 0
+while i < r :
+    a, v, d = texto[i].split()
+    assassinos.append(a)
+    vitimas.append(v)
+    detetives.append(d)
+    i += 1
+
+assassinos.sort()
+vitimas.sort()
+detetives.sort()
+todomundo = assassinos + vitimas + detetives
+todomundo = list(set(todomundo))
+todomundo.sort()
+
