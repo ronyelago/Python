cfa98d2a1fb4f4b6d4f9927a104611e20b63cbe2
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-12 00:24:25 -0300

    lists

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-assassino, vitima, detetive = input("digite três nomes: ").split()
+texto = open(r"C:\Workspace\Python\Python\arqs-06\arq4.in").readlines()
+r = int(texto[0])
 
-print(assassino)
-print(vitima)
-print(detetive)
+assassinos = []
+vitimas = []
+detetives = []
+
+for i in range(r) :
+    assassinos = texto[1]
+    detetives = texto[2]
+    vitimas = texto[3]
